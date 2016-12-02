3cb7db539b812df87ca9f0e2ba1fc287c52c9c6c
Rahul <rahul444@hotmail.com>
2016-12-02 15:54:57 -0800

    transforms input file into matrix

---
+++ dfs_algo.py
@@ -0,0 +1,11 @@
+input_file = open('./in/13.in')
+lines = input_file.readlines()
+
+V = int(lines[0][0:1])
+matrix = [[0 for x in range(V)] for y in range(V)]
+
+for i in range(1, V):
+    line = lines[i].split()
+    for j in range(len(line)):
+        matrix[i-1][j] = int(line[j])
+print(matrix)
